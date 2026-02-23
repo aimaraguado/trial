2a2c93159a6036013609159576ddb8bc97378b13
aimaraguado <aimar.aguado@multiversecomputing.com>
2026-02-23 11:51:24 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -65,5 +65,30 @@ clf.fit(X,y)
 predict = clf.predict(input_row)
 predict_proba = clf.predict_proba(input_row)
 st.subheader("Predicted Species")
+st.dataframe(predict_proba,
+            column_config = {
+              "Adelie": st.column_config.ProgressColumn(
+                "Adelie",
+                format="%f",
+                width="medium",
+                min_value=0,
+                max_value=1
+              ),
+              "Chinstrap": st.column_config.ProgressColumn(
+                "Chinstrap",
+                format="%f",
+                width="medium",
+                min_value=0,
+                max_value=1
+              ),
+              "Gentoo": st.column_config.ProgressColumn(
+                "Gentoo",
+                format="%f",
+                width="medium",
+                min_value=0,
+                max_value=1
+              )
+            })
+
 penguins_species = np.array(["Adelie", "Chinstrap", "Gentoo"])
 st.success(penguins_species[predict][0])
