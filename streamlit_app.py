82f52e10198c0082470b48a85723329c73186923
aimaraguado <aimar.aguado@multiversecomputing.com>
2026-02-23 10:12:46 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,3 +17,5 @@ with st.expander("Data"):
   st.write("**Y**")
   Y = df.species
   Y
+with st.expander("Data Visualization"):
+  st.scatter_chart(data=df, x = "bill_length_mm", y = "body_mass_g", color = "species")
