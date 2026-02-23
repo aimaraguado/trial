3f79894d4b43724434ecc77afc99e32567a47831
aimaraguado <aimar.aguado@multiversecomputing.com>
2026-02-23 12:50:34 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,21 +23,30 @@ with st.expander("Data Visualization"):
   st.scatter_chart(data=df, x = "bill_length_mm", y = "body_mass_g", color = "species")
 
 with st.sidebar:
-  st.header("Input features")
-  island = st.selectbox("Island", ("Biscoe", "Dream", "Togersen"))
-  gender = st.selectbox("Gender", ("male", "female"))
-  bill_length_mm = st.slider("Bill length (mm)", 32.1, 60.0, 45.0)
-  bill_depth_mm = st.slider("Bill depth (mm)", 13.0, 22.0, 17.0)
-  flicker_length_mm = st.slider("Flicker length (mm)", 172.0, 231.0, 201.0)
-  body_mass_g = st.slider("Body mass (g)", 2700.0, 6300.0, 4700.0 )
-  data = {"island": island,
-         "bill_length_mm": bill_length_mm,
-         "bill_depth_mm": bill_depth_mm,
-         "flicker_length_mm": flicker_length_mm,
-         "body_mass_g": body_mass_g,
-         "sex": gender}
-  input_df = pd.DataFrame(data, index = [0])
-  input_penguins = pd.concat([input_df, X], axis = 0)
+    st.header("Input features")
+
+    with st.form("penguin_form"):
+        island = st.selectbox("Island", ("Biscoe", "Dream", "Togersen"))
+        gender = st.selectbox("Gender", ("male", "female"))
+
+        bill_length_mm = st.slider("Bill length (mm)", 32.1, 60.0, 45.0)
+        bill_depth_mm = st.slider("Bill depth (mm)", 13.0, 22.0, 17.0)
+        flipper_length_mm = st.slider("Flipper length (mm)", 172.0, 231.0, 201.0)
+        body_mass_g = st.slider("Body mass (g)", 2700.0, 6300.0, 4700.0)
+
+        submitted = st.form_submit_button("🔮 Predict species")
+  if submitted:
+    data = {
+        "island": island,
+        "bill_length_mm": bill_length_mm,
+        "bill_depth_mm": bill_depth_mm,
+        "flipper_length_mm": flipper_length_mm,
+        "body_mass_g": body_mass_g,
+        "sex": gender
+    }
+
+    input_df = pd.DataFrame(data, index=[0])
+    input_penguins = pd.concat([input_df, X], axis = 0)
 
   encode = ["island", "sex"]
   df_penguins = pd.get_dummies(input_penguins, prefix = encode)
