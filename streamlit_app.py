4755a71b43f80f45f875837a0ccab17431bfd7c1
aimaraguado <aimar.aguado@multiversecomputing.com>
2026-02-23 10:22:25 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,4 +28,11 @@ with st.sidebar:
   bill_depth_mm = st.slider("Bill depth (mm)", 13.0, 22.0, 17.0)
   flicker_length_mm = st.slider("Flicker length (mm)", 172.0, 231.0, 201.0)
   body_mass_g = st.slider("Body mass (g)", 2700.0, 6300.0, 4700.0 )
-  
+  data = {"island", island,
+         "bill_length_mm", bill_length_mm,
+         "bill_depth_mm", bill_depth_mm,
+         "flicker_length_mm", flicker_length_mm,
+         "body_mass_g", body_mass_g,
+         "gender", gender}
+  input_df = pd.DataFrame(data, index = [0])
+  input_df
